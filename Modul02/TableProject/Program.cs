554af78e224ba9d494bfbd43d0c35415a27f3cda
Петър Петров <peter_petrov@gbg.bg>
2015-11-15 12:53:41 +0200

    Модул 02, Видео 09 - Работа с enum

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -4,6 +4,9 @@ namespace TableProject
 {
 	class MainClass
 	{
+		enum ti {ime = 0, familia, godini};
+
+
 		public static void Main (string[] args)
 		{
 
@@ -11,17 +14,20 @@ namespace TableProject
 			string[,] table = new string[2, 3];
 
 			//Въвеждане на първи ред
-			table[0, 0] = "Петър"; table[0, 1] = "Петров"; table[0, 2] = "52";
+			table[0, (int) ti.ime] = "Петър"; table[0, (int) ti.familia] = "Петров"; table[0, (int) ti.godini] = "52";
 
 			//Въвеждане на втори ред
-			table[1, 0] = "Петров"; table[1, 1] = "Петър"; table[0, 2] = "50";
+			table[1, (int) ti.ime] = "Петров"; table[1, (int) ti.familia] = "Петър"; table[0, (int) ti.godini] = "50";
 
 
 			//Печат
 			Console.Write ("Кой ред искате да видите: ");
 			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
 
-			Console.WriteLine ((table[_index,0])[0] + ". " + table[_index,1] + ", " + table[_index,2] + " г.");
+			Console.WriteLine (
+				(table[_index, (int) ti.ime])[0] + ". " + 
+				 table[_index, (int) ti.familia] + ", " + 
+				 table[_index, (int) ti.godini] + " г.");
 
 
 		}
